cada94a089b559f5fa5eeeff849e9c8df3565bdd
anhmonk <anhmonk@gmail.com>
2023-06-01 12:22:59 +0700

    Project init

---
+++ BirdClinic/src/main/java/com/birdclinic/BirdClinic/models/home/HomePageDAOImpl.java
@@ -0,0 +1,76 @@
+package com.birdclinic.BirdClinic.models.home;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.sql.Types;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HomePageDAOImpl implements HomePageDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Map<Integer, String>> getPageHeaders() {
+        final String PAGE_HEADERS = "select HomePageID, HomePageHeader " +
+                "from Homepage";
+        ArrayList<Map<Integer, String>> pageHeadArrayList = new ArrayList<>();
+
+        try {
+            jdbcTemplate.query(
+                    PAGE_HEADERS,
+                    new Object[0],
+                    new int[0],
+                    (ResultSetExtractor<Object>) (rs) -> {
+                        while (rs.next()) {
+                            Map<Integer, String> pageHead = new HashMap<Integer, String>();
+                            pageHead.put(rs.getInt("HomePageID"), rs.getString("HomePageHeader"));
+                            pageHeadArrayList.add(pageHead);
+                        }
+                        return new Object();
+                    });
+        } catch (EmptyResultDataAccessException ex) {
+            ex.printStackTrace();
+        }
+
+        return pageHeadArrayList;
+    }
+
+    @Override
+    public HomePageDTO getPage(int homePageID) {
+        final String PAGE_CONTENT = "select HomePageContent, HomePageID, HomePageHeader " +
+                "from Homepage " +
+                "where HomePageID = ?";
+
+        HomePageDTO homePage = null;
+
+        try {
+            homePage = jdbcTemplate.query(
+                    PAGE_CONTENT,
+                    new Object[] { Integer.valueOf(homePageID) },
+                    new int[] { Types.INTEGER },
+                    (ResultSetExtractor<HomePageDTO>) (rs) -> {
+                        HomePageDTO page = null;
+                        while (rs.next()) {
+                            page = new HomePageDTOImpl();
+                            page.setHomePageID(rs.getInt("HomePageID"));
+                            page.setHomePageContent(rs.getString("HomePageContent"));
+                            page.setHomePageHeader(rs.getString("HomePageHeader"));
+                        }
+                        return page;
+                    });
+        } catch (EmptyResultDataAccessException ex) {
+            ex.printStackTrace();
+        }
+
+        return homePage;
+    }
+}
