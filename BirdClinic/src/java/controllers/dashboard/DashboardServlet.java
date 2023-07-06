852102ef6d322c4ebb7c169666062e3d84c08a6d
anhmonk <anhmonk@gmail.com>
2023-07-06 15:27:45 +0700

    Booking fix 5

--- BirdClinic/src/java/controllers/dashboard/DashboardServlet.java
+++ BirdClinic/src/java/controllers/dashboard/DashboardServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import services.general.GeneralServices;
 
 /**
  *
@@ -30,7 +32,14 @@ public class DashboardServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
         
+        GeneralServices services = (GeneralServices) session.getAttribute("service");
+        String userRole = services.getCurrentUser().getDisplayRole();
+        
+        String url = "/" + userRole + request.getRequestURI();
+
+        request.getRequestDispatcher(url).forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
