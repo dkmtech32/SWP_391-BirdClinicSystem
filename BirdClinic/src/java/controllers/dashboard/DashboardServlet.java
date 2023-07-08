80f79a6c6bfb3157bc44d5ac35747020bd5a0264
dkmtech32 <109049675+dkmtech32@users.noreply.github.com>
2023-07-08 17:12:28 +0700

    update dashboard

--- BirdClinic/src/java/controllers/dashboard/DashboardServlet.java
+++ BirdClinic/src/java/controllers/dashboard/DashboardServlet.java
@@ -48,7 +48,7 @@ public class DashboardServlet extends HttpServlet {
         }
 
         request.getRequestDispatcher(forwardURL).include(request, response);
-        request.getRequestDispatcher("Common/dashboard.jsp").forward(request, response);
+        request.getRequestDispatcher("../Common/dashboard.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
